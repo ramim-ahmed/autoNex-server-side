e8a2745ae08da80580240340a368f7a544698bf1
ramim-ahmed <ramimahmedraj2@gmail.com>
2024-05-26 14:16:16 +0600

    [DONE] category-model-and-schema-creation

---
+++ src/app/modules/category/category.model.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require("mongoose");
+
+const categorySchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    timestamps: true,
+  }
+);
+
+const Category = model("Category", categorySchema);
+
+module.exports = Category;
