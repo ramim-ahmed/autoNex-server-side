8cc571805690a36991b7f5a8c4ab7f86f0056fb1
ramim-ahmed <ramimahmedraj2@gmail.com>
2024-05-26 17:45:28 +0600

    [DONE] product-service-functionality-with-database

---
+++ src/app/modules/product/product.service.js
@@ -0,0 +1,34 @@
+const Product = require("./product.model");
+
+const createNewProduct = async (data) => {
+  const result = await Product.create(data);
+  return result;
+};
+
+const getAllProducts = async () => {
+  const result = await Product.find({}).sort({ createdAt: "desc" });
+  return result;
+};
+
+const getSingleProduct = async (id) => {
+  const result = await Product.findOne({ _id: id });
+  return result;
+};
+
+const updateProduct = async (id, data) => {
+  const result = await Product.updateOne({ _id: id }, data);
+  return result;
+};
+
+const deleteProduct = async (id) => {
+  const result = await Product.deleteOne({ _id: id });
+  return result;
+};
+
+module.exports.productService = {
+  createNewProduct,
+  getAllProducts,
+  getSingleProduct,
+  updateProduct,
+  deleteProduct,
+};
